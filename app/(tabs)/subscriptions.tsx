be95ca1af014e0fb7005df399de6b5f66ab95c81
sudhabanerjeecc <sudha.banerjee@codeclouds.in>
2026-04-26 20:47:00 +0530

    feat: file update

--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 const Subscriptions = () => {
   return (
     <View>
-      <Text>Subscriptions</Text>
+      <Text>Subscriptions Page</Text>
     </View>
   );
 };
