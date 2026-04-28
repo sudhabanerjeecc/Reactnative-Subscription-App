16c8e1b3f83d245eb061431acac602f7b2a68676
sudhabanerjeecc <sudha.banerjee@codeclouds.in>
2026-04-28 14:36:45 +0530

    feat: Add tabs features route

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
@@ -5,6 +6,9 @@ const SignIn = () => {
   return (
     <View>
       <Text>Sign In page</Text>
+      <Link className="mt-50" href={"/"}>
+        Go back
+      </Link>
     </View>
   );
 };
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
@@ -5,6 +6,9 @@ const SignUp = () => {
   return (
     <View>
       <Text>SignUp page</Text>
+      <Link className="mt-50" href={"/"}>
+        Go back
+      </Link>
     </View>
   );
 };
