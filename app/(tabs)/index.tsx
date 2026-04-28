6b3e7cf3869dd9b2e4278ce6b14f8ffd8672849e
sudhabanerjeecc <sudha.banerjee@codeclouds.in>
2026-04-28 14:36:45 +0530

    feat: Add tabs features route

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import "@/global.css";
 import { Link } from "expo-router";
+import { styled } from "nativewind";
 import { Text, View } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="flex-1  bg-background p-5">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
@@ -35,6 +39,6 @@ export default function Index() {
           Go to Claude Subscription
         </Link>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+
+import { styled } from "nativewind";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 const Subscriptions = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1  bg-background p-5">
       <Text>Subscriptions Page</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
